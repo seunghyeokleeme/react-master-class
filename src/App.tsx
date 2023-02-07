f61df50d2c983b8d55f84b04c99144ae67a1b569
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-02-07 19:29:48 +0900

    themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,27 @@
 import { useState } from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${props => props.theme.textColor};
+`;
+
+//  bgColor: "white",
+//   textColor: "black",
+//   btnColor: "tomato",
+
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={value}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Hello React.js</H1>
+    </Container>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+const lightTheme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+  btnColor: "tomato",
+};
+
+const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+  btnColor: "teal",
+};
+
+export { lightTheme, darkTheme };
