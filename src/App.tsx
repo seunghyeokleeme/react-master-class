7dd62b8831f0d4d2d417a8a65d316f1eeb17d2ab
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-02-07 16:56:28 +0900

    Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import styled from "styled-components";
-import Circle from "./Circle";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
     <div>
-      <Circle borderColor="black" bgColor="tomato" />
-      <Circle bgColor="teal" />
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={value}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
