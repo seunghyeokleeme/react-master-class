6109428058525b44905528ae8a08cc180ae33c32
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-02-10 15:06:55 +0900

    Feat price chart (#2)
    * feat(Chart): Coin history 데이터 api 추가
    
    api.tsx에 fetchCoinHistory api 생성
    
    See also: NomadCoder react-master 5.12
    
    * feat(Chart) 차트 구현
    
    See also: Nomadcoder react-master 5.13
    
    * feat(Chart): Chart 스타일 추가
    
    See also: NomadCoder React-master 5.14

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,83 @@
-function Chart() {
-  return <h1>Chart</h1>;
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface ChartProps {
+  coinId: string;
+}
+
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading Chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "price",
+              data: data?.map(price => +price.close).slice(0, 15) ?? [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 5,
+            },
+            yaxis: { show: false },
+            xaxis: {
+              axisTicks: { show: false },
+              axisBorder: { show: false },
+              labels: { show: false },
+              type: "datetime",
+              categories:
+                data
+                  ?.map(price =>
+                    new Date(price.time_close * 1000).toISOString()
+                  )
+                  .slice(0, 15) ?? [],
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: value => `$${value.toFixed(3)}`,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
