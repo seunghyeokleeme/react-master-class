bc0737e90ed445e0aee1aa62ecd028c21e8ba29e
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-02-18 05:40:05 +0900

    첼린지 초안 제출

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,21 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: space-around;
   align-items: center;
+  flex-direction: column;
 `;
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
   width: 50vw;
   gap: 10px;
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
-  }
 `;
 
 const Box = styled(motion.div)`
-  height: 400px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
+  height: 340px;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -41,15 +39,62 @@ const Overlay = styled(motion.div)`
   align-items: center;
 `;
 
+const Circle = styled(motion.div)`
+  background-color: white;
+  width: 100px;
+  height: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  border-radius: 50%;
+`;
+
+const Btn = styled(motion.button)``;
+
 function App() {
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked(prev => !prev);
   const [id, setId] = useState<null | string>(null);
   return (
     <Wrapper>
       <Grid>
-        {["1", "2", "3", "4"].map(n => (
-          <Box onClick={() => setId(n)} key={n} layoutId={n} />
-        ))}
+        <Box
+          onClick={() => setId("1")}
+          layoutId="1"
+          whileHover={{ x: -10, y: -10 }}
+        ></Box>
+        <Box
+          onClick={() => setId("2")}
+          layoutId="2"
+          whileHover={{ x: 10, y: -10 }}
+        >
+          {!clicked ? <Circle layoutId="circle" /> : null}
+        </Box>
+        <Box
+          onClick={() => setId("3")}
+          layoutId="3"
+          whileHover={{ x: -10, y: 10 }}
+        >
+          {clicked ? <Circle layoutId="circle" /> : null}
+        </Box>
+        <Box
+          onClick={() => setId("4")}
+          layoutId="4"
+          whileHover={{ x: 10, y: 10 }}
+        ></Box>
       </Grid>
+      <Btn
+        onClick={toggleClicked}
+        style={{
+          background: "white",
+          border: "none",
+          borderRadius: 10,
+          width: 80,
+          height: 30,
+        }}
+        initial={{ color: "rgb(0, 0, 255)", scale: 1 }}
+        whileTap={{ color: "rgb(255, 165, 0)", scale: 1.5 }}
+      >
+        Switch
+      </Btn>
       <AnimatePresence>
         {id ? (
           <Overlay
@@ -58,7 +103,10 @@ function App() {
             animate={{ backgroundColor: "rgba(0, 0, 0, 0.8)" }}
             exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
           >
-            <Box layoutId={id} style={{ width: 400, height: 200 }} />
+            <Box
+              layoutId={id}
+              style={{ width: 400, height: 400, background: "white" }}
+            />
           </Overlay>
         ) : null}
       </AnimatePresence>
